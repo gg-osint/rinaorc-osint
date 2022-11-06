a91c9cea85f2352a22e3b5a31b0e98367819ed50
Mat <87903769+MathisDevelopment@users.noreply.github.com>
2022-11-06 13:42:58 +0100

    📌 Add another informations

--- index.js
+++ index.js
@@ -4,20 +4,33 @@ const readline = require('readline')
 const cmd = readline.createInterface(process.stdin, process.stdout)
 const chalk = require('chalk')
 
-console.log("d8888b. d888888b d8b   db  .d8b.   .d88b.  d8888b.  .o88b.          .d88b.  .d8888. d888888b d8b   db d888888b \n" +
-  "88  `8D   `88'   888o  88 d8' `8b .8P  Y8. 88  `8D d8P  Y8         .8P  Y8. 88'  YP   `88'   888o  88 `~~88~~' \n" +
-  "88oobY'    88    88V8o 88 88ooo88 88    88 88oobY' 8P              88    88 `8bo.      88    88V8o 88    88    \n" +
-  "88`8b      88    88 V8o88 88~~~88 88    88 88`8b   8b              88    88   `Y8b.    88    88 V8o88    88    \n" +
-  "88 `88.   .88.   88  V888 88   88 `8b  d8' 88 `88. Y8b  d8         `8b  d8' db   8D   .88.   88  V888    88    \n" +
-  "88   YD Y888888P VP   V8P YP   YP  `Y88P'  88   YD  `Y88P'          `Y88P'  `8888Y' Y888888P VP   V8P    YP    \n" +
-  "\n" +
+console.log(chalk.yellowBright(" 888888ba  oo                                                   dP                     dP          \n" +
+  " 88    `8b                                                      88                     88          \n" +
+  "a88aaaa8P' dP 88d888b. .d8888b. .d8888b. 88d888b. .d8888b.    d8888P .d8888b. .d8888b. 88 .d8888b. \n" +
+  " 88   `8b. 88 88'  `88 88'  `88 88'  `88 88'  `88 88'  `\"\"      88   88'  `88 88'  `88 88 Y8ooooo. \n" +
+  " 88     88 88 88    88 88.  .88 88.  .88 88       88.  ...      88   88.  .88 88.  .88 88       88 \n" +
+  " dP     dP dP dP    dP `88888P8 `88888P' dP       `88888P'      dP   `88888P' `88888P' dP `88888P' \n"))
 
-  "Par Mat'#9885 - discord.gg/osint" +
-  "\n\n")
+console.log("Par Mat'#9885 - discord.gg/osint\n\n")
 
-cmd.question('Entrez le pseudo : ', (pseudo) => {
+
+
+cmd.question('Pseudo de la cible : ', (pseudo) => {
   async function main () {
     try {
+      await axios({
+        url: `https://api.rinaorc.com/player/shika258`,
+        method: 'GET',
+        headers: { 'API-Key': config['api-token'] }
+      })
+    } catch (error) {
+      return console.log(`\n[${chalk.redBright('-')}] Erreur: ` + `La clef d'API fournit n'est pas valide`)
+    }
+    try {
+
+      let startTime = new Date().getTime()
+      let elapsedTime = 0
+
       const playerData = await axios({
         url: `https://api.rinaorc.com/player/${pseudo}`,
         method: 'GET',
@@ -25,27 +38,38 @@ cmd.question('Entrez le pseudo : ', (pseudo) => {
           'API-Key': config['api-token']
         }
       })
-      console.log(`\n[${chalk.greenBright("+")}] Pseudo: ` + playerData.data.player.name)
-      console.log(`[${chalk.greenBright("+")}] Rang: ` + playerData.data.player.rank.name)
-      let hasBoost = playerData.data.player.hasBoost;
-      if (hasBoost === true) hasBoost = "Oui"; else hasBoost = "Non";
-      console.log(`[${chalk.greenBright("+")}] Booster: ` + hasBoost)
-      let connectionMethod = playerData.data.player.connectionMethod;
-      if (connectionMethod === 'CRACK') connectionMethod = "Crack"; else if (connectionMethod === 'PREMIUM') connectionMethod = "Premium"; else connectionMethod = "Inconnu";
-      console.log(`[${chalk.greenBright("+")}] Type de compte: ` + connectionMethod)
-      if(connectionMethod === "Premium") {
-        console.log(`   [${chalk.blueBright("*")}] UUID: ` + (await axios({ url: `https://minecraft-api.com/api/uuid/${playerData.data.player.name}`, method: 'GET', })).data)
-        console.log(`   [${chalk.blueBright("*")}] Skin: ` + `https://minecraft-api.com/api/skins/${playerData.data.player.name}/body/10.5/10/`)
-        console.log(`   [${chalk.blueBright("*")}] Tête: ` + `https://minecraft-api.com/api/skins/${playerData.data.player.name}/head/10.5/10/`)
-      }
-      let discordAsLink = playerData.data.player.links.discord;
-      if (discordAsLink === null) discordAsLink = "Aucun compte lié"; else discordAsLink = `${discordAsLink}`
-      console.log(`\n[${chalk.greenBright("+")}] Compte Discord: ` + discordAsLink)
 
-      if(playerData.data.player.links.discord !== null) {
-        if(config['discord-bot-token'] === "") {
-          console.log(`   [${chalk.redBright("-")}] Vous devez ajouter un token de bot discord dans le fichier config.json pour pouvoir avoir les informations du compte discord`)
-        } else {
+      console.log(`\n[${chalk.greenBright('+')}] Pseudo: ` + playerData.data.player.name)
+      console.log(`[${chalk.greenBright('+')}] Rang: ` + playerData.data.player.rank.name)
+
+      let hasBoost = playerData.data.player.hasBoost
+      if (hasBoost === true) hasBoost = 'Oui'; else hasBoost = 'Non'
+      console.log(`[${chalk.greenBright('+')}] Booster: ` + hasBoost)
+      console.log(`   [${chalk.blueBright('*')}] Temps de boost total: ` + playerData.data.player.boostMonths + ' mois \n')
+
+      let connectionMethod = playerData.data.player.connectionMethod
+      if (connectionMethod === 'CRACK') connectionMethod = 'Crack'; else if (connectionMethod === 'PREMIUM') connectionMethod = 'Premium'; else connectionMethod = 'Inconnu'
+      console.log(`[${chalk.greenBright('+')}] Type de compte: ` + connectionMethod)
+
+      if (connectionMethod === 'Premium') {
+        try {
+          console.log(`   [${chalk.blueBright('*')}] UUID: ` + (await axios({
+            url: `https://minecraft-api.com/api/uuid/${playerData.data.player.name}`,
+            method: 'GET'
+          })).data)
+          console.log(`   [${chalk.blueBright('*')}] Skin: ` + `https://minecraft-api.com/api/skins/${playerData.data.player.name}/body/10.5/10/`)
+          console.log(`   [${chalk.blueBright('*')}] Tête: ` + `https://minecraft-api.com/api/skins/${playerData.data.player.name}/head/10.5/10/`)
+        } catch (err) {
+          console.log(`   [${chalk.redBright('-')}] Erreur lors de la récupération des informations du compte Minecraft`)
+        }
+      }
+
+      let discordAsLink = playerData.data.player.links.discord
+      if (discordAsLink === null) discordAsLink = 'Aucun compte lié'; else discordAsLink = `${discordAsLink}`
+      console.log(`\n[${chalk.greenBright('+')}] Compte Discord: ` + discordAsLink)
+
+      if (playerData.data.player.links.discord !== null) {
+        try {
           const discordData = await axios({
             url: `https://discord.com/api/v9/users/${discordAsLink}`,
             method: 'GET',
@@ -53,17 +77,24 @@ cmd.question('Entrez le pseudo : ', (pseudo) => {
               Authorization: `Bot ${config['discord-bot-token']}`
             }
           })
-          console.log(`   [${chalk.blueBright("*")}] Pseudo: ` + discordData.data.username)
-          console.log(`   [${chalk.blueBright("*")}] Discriminateur: ` + discordData.data.discriminator)
-          console.log(`   [${chalk.blueBright("*")}] Avatar: ` + `https://cdn.discordapp.com/avatars/${discordData.data.id}/${discordData.data.avatar}`)
-          console.log(`   [${chalk.blueBright("*")}] Bannière: ` + `https://cdn.discordapp.com/banners/${discordData.data.id}/${discordData.data.banner}`)
-          console.log(`   [${chalk.blueBright("*")}] Couleur de bannière: ` + discordData.data.banner_color)
-          console.log(`   [${chalk.blueBright("*")}] Couleur d'accentuation: ` + discordData.data.accent_color)
-          console.log(`   [${chalk.blueBright("*")}] Flags public: ` + discordData.data.public_flags)
+          console.log(`   [${chalk.blueBright('*')}] Pseudo: ` + discordData.data.username)
+          console.log(`   [${chalk.blueBright('*')}] Discriminateur: ` + discordData.data.discriminator)
+          console.log(`   [${chalk.blueBright('*')}] Avatar: ` + `https://cdn.discordapp.com/avatars/${discordData.data.id}/${discordData.data.avatar}`)
+          console.log(`   [${chalk.blueBright('*')}] Bannière: ` + `https://cdn.discordapp.com/banners/${discordData.data.id}/${discordData.data.banner}`)
+          console.log(`   [${chalk.blueBright('*')}] Couleur de bannière: ` + discordData.data.banner_color)
+          console.log(`   [${chalk.blueBright('*')}] Couleur d'accentuation: ` + discordData.data.accent_color)
+          console.log(`   [${chalk.blueBright('*')}] Flags public: ` + discordData.data.public_flags)
+        } catch {
+          console.log(`   [${chalk.redBright('-')}] Impossible de récupérer les informations du compte discord, cela peut être dû à un token invalide ou à un compte banni`)
         }
       }
+
+      elapsedTime = new Date().getTime() - startTime
+      console.log(`\n\nCe script a été exécuté en ${elapsedTime / 1000} seconde.`)
+      cmd.close()
+
     } catch (error) {
-      console.log(`\n[${chalk.redBright("-")}] Erreur: ` + "Le pseudo n'existe pas ou le compte n'est pas lié à Rinaorc.")
+      console.log(`\n[${chalk.redBright('-')}] Erreur: ` + 'Le pseudo n\'existe pas ou le compte n\'est pas lié à Rinaorc.')
     }
   }
 
